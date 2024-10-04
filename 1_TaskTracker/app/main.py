cae266cd57b2f8d74c8e89960d64a153ec552002
butakovbi <butakovbi@gmail.com>
2024-10-04 22:30:20 +0300

    auth using fastapi-users added

--- 1_TaskTracker/app/main.py
+++ 1_TaskTracker/app/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
 
-app = FastAPI(title='Trading App')
+app = FastAPI(
+    title='TaskTracker'
+)
 
-@app.get("/users")
-async def get_user():
-    return {"message": "test"}
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
---
+++ learn_project/main.py
@@ -0,0 +1,54 @@
+from fastapi import Depends, FastAPI
+from fastapi_users import FastAPIUsers, fastapi_users
+import uvicorn
+
+from auth.auth import auth_backend
+from auth.database import User
+from auth.manager import get_user_manager
+from auth.schemas import UserCreate, UserRead
+
+app = FastAPI(title='Trading App')
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+
+current_user = fastapi_users.current_user()
+
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.username}"
+
+@app.get("/unprotected-route")
+def unprotected_route():
+    return f"Hello, anonym"
+    
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', reload=True)

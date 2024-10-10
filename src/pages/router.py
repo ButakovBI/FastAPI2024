60df7ea30de103cfe5853dbc48b4cc22cbbd82e4
butakovbi <butakovbi@gmail.com>
2024-10-10 23:32:51 +0300

    updated requirements.txt, linter fixes, delete and untrack .idea files

--- src/pages/router.py
+++ src/pages/router.py
@@ -1,12 +1,10 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
-router = APIRouter(
-    prefix="/pages",
-    tags=["Pages"]
-)
+router = APIRouter(prefix="/pages", tags=["Pages"])
 
 templates = Jinja2Templates(directory="templates")
+
 
 @router.get("/")
 def get_root_page(request: Request):

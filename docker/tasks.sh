5e1f88272268ff9c19f6e7ff0e0f424f95350004
butakovbi <butakovbi@gmail.com>
2024-11-14 00:09:38 +0300

    big progress in k8s

--- docker/tasks.sh
+++ docker/tasks.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+source /path/to/.env
+
 cd src
 
 if [[ "${1}" == "celery" ]]; then
   celery --app=tasks.tasks:celery worker -l INFO
 elif [[ "${1}" == "flower" ]]; then
-  celery --app=tasks.tasks:celery flower
- fi
+  celery --app=tasks.tasks:celery flower --broker="${CELERY_BROKER_URL}"
+fi
